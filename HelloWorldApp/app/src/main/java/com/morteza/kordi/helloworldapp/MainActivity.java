ac6945fee2bd8c0d132a14054f55c006d20dabc0
Nikola901 <stevebienaime@gmail.com>
2019-03-09 19:59:48 -0500

    adding an imageview

--- HelloWorldApp/app/src/main/java/com/morteza/kordi/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/morteza/kordi/helloworldapp/MainActivity.java
@@ -5,20 +5,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
      void ButtonIsClicked(View buttonView){
          int result = 5*7;
-         //TextView showtext = findViewById(R.id.textView);
-          TextView edtName = findViewById(R.id.editText);
 
-         Log.i("OPERATION", edtName.getText().toString());
+         // links TextView UI component to code via id
+         TextView showtext = findViewById(R.id.textView);
+         TextView edtName = findViewById(R.id.editText2);
+         //sends messages to logcat under information
+        Log.i("OPERATION", edtName.getText().toString());
 
 
-
-
+         Toast.makeText(MainActivity.this, showtext.getText().toString(), Toast.LENGTH_LONG).show();
 
      }
+
 
 
     @Override
